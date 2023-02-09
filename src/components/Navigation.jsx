2e9107d62822740d875f3a9b99b769665a2ad10c
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-09 19:40:29 +0100

    design: footer

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,88 +7,59 @@ export default function Navigation() {
       <nav
         className="
             w-full
-            flex flex-wrap
-            items-center
-            justify-between
             py-4
             bg-gray-100
             text-gray-500
             hover:text-gray-700
             focus:text-gray-700
             shadow-lg
-            navbar navbar-expand-lg navbar-light
             "
       >
-        <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-          <button
-            className="
-                navbar-toggler
-                text-gray-500
-                border-0
-                hover:shadow-none hover:no-underline
-                py-2
-                px-2.5
-                bg-transparent
-                focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline
-              "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="bars"
-              className="w-6"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-              ></path>
-            </svg>
-          </button>
+        <div className=" w-full flex items-center justify-between px-6">
           <div
-            className="collapse navbar-collapse flex-grow items-center"
+            className=" flex items-center justify-between"
             id="navbarSupportedContent"
           >
-            <Link className="text-xl text-black" to="/">
-              Navbar
-            </Link>
-            <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+            <div className="logo">
+              <Link to="/" className="text-xl text-black">
+                Alexis Noukan
+              </Link>
+            </div>
+          </div>
+          <div>
+            <ul className="navbar-nav flex items-center justify-center  pl-0 list-style-none mr-auto">
               <li className="nav-item px-2">
-                <Link className="nav-link active" aria-current="page" to="/">
-                  Home
+                <Link to="/" className="nav-link active" aria-current="page">
+                  <span>Home</span>
                 </Link>
               </li>
-              <li className="nav-item pr-2">
+              <li className="nav-item px-2">
                 <Link
-                  className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
-                  to="/"
+                  to="/Work"
+                  className="nav-link active"
+                  aria-current="page"
                 >
-                  Features
+                  <span>Past Work</span>
                 </Link>
               </li>
-              <li className="nav-item pr-2">
+              <li className="nav-item px-2">
                 <Link
-                  className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
-                  to="/"
+                  to="/Knowledge"
+                  className="nav-link active"
+                  aria-current="page"
                 >
-                  Pricing
-                </Link>
-              </li>
-              <li className="nav-item pr-2">
-                <Link to="/" className="nav-link disabled text-gray-300 p-0">
-                  Disabled
+                  <span>Knowledge</span>
                 </Link>
               </li>
             </ul>
+          </div>
+          <div className=" flex gap-5">
+            <div>
+              <Link to="/Contact">Contact now</Link>
+            </div>
+            <div>
+              <button>light / dark</button>
+            </div>
           </div>
         </div>
       </nav>
