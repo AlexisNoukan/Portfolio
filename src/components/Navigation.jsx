486e621455f54e540c59dc323248990f165a27a2
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-09 21:22:43 +0100

    design: night mode

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -75,7 +75,7 @@ export default function Navigation() {
               </li>
             </ul>
           </div>
-          <div className=" flex gap-5">
+          <div className=" flex gap-5 items-center ">
             <div>
               <Link to="/Contact">Contact now</Link>
             </div>
