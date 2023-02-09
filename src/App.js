a17d6469e2ffb49a1574f8d034a1cb34781de0df
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-09 09:00:01 +0100

    update: link in to the nav

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Work from "./pages/Work";
 import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Work" element={<Work />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Work" element={<Work />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -17,33 +18,37 @@ export default function Navbar() {
     navbar navbar-expand-lg navbar-light"
     >
       <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-        <a class="text-xl text-black" href="#">
+        <Link to="/" class="text-xl text-black" href="#">
           Navbar
-        </a>
+        </Link>
         <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
           <li class="nav-item px-2">
-            <a class="nav-link active" aria-current="page" href="#">
+            <Link to="/" class="nav-link active" aria-current="page" href="#">
               Home
-            </a>
+            </Link>
           </li>
           <li class="nav-item pr-2">
-            <a
+            <Link
+              to="/"
               class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
               href="#"
             >
               Features
-            </a>
+            </Link>
           </li>
           <li class="nav-item pr-2">
-            <a
+            <Link
+              to="/"
               class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
               href="#"
             >
               Pricing
-            </a>
+            </Link>
           </li>
           <li class="nav-item pr-2">
-            <a class="nav-link disabled text-gray-300 p-0">Disabled</a>
+            <Link to="/" class="nav-link disabled text-gray-300 p-0">
+              Disabled
+            </Link>
           </li>
         </ul>
       </div>
