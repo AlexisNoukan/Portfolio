d9171c2836a7352cf2933ea5c7297c244b277081
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-09 18:11:31 +0100

    fix: description size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       <div className=" flex justify-center">
-        <div className="bg-white flex w-3/6 dark:bg-gray-800 pt-20 mt-32 rounded-xl shadow-lg shadow-indigo-500/50  ">
+        <div className="bg-white flex  dark:bg-gray-800 pt-20 mt-32 rounded-xl shadow-lg shadow-indigo-500/50  ">
           <div className=" w-1/2 mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
             <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
               <span className="block text-6xl">I am Alexis</span>
@@ -21,7 +21,7 @@ const Home = () => {
             </p>
 
             <div className="lg:mt-0 lg:flex-shrink-0">
-              <div class="mt-12 inline-flex rounded-md shadow">
+              <div className="mt-12 inline-flex rounded-md shadow">
                 <button
                   type="button"
                   className="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
