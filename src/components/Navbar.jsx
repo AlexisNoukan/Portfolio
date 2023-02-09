2df687736268fd0f03573a3becf57c1131f7c153
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-09 08:54:57 +0100

    add: nav code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,49 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div>
-      <div className="logo">
-        <h1>LOGO</h1>
-      </div>
-      <div className="nav">
-        <ul>
-          <li>Home</li>
-          <li>Past Work</li>
-          <li>Contact</li>
+    <div
+      className="relative
+    w-full
+    flex flex-wrap
+    items-center
+    justify-between
+    py-4
+    bg-gray-100
+    text-gray-500
+    hover:text-gray-700
+    focus:text-gray-700
+    shadow-lg
+    navbar navbar-expand-lg navbar-light"
+    >
+      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+        <a class="text-xl text-black" href="#">
+          Navbar
+        </a>
+        <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+          <li class="nav-item px-2">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item pr-2">
+            <a
+              class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
+              href="#"
+            >
+              Features
+            </a>
+          </li>
+          <li class="nav-item pr-2">
+            <a
+              class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
+              href="#"
+            >
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item pr-2">
+            <a class="nav-link disabled text-gray-300 p-0">Disabled</a>
+          </li>
         </ul>
       </div>
     </div>
