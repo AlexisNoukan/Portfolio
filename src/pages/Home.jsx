19d835c788c23319bf26ea50c72b0a3a5137f486
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-10 13:54:24 +0100

    end: home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,10 @@ const Home = () => {
   return (
     <div>
       <div className=" flex justify-center">
-        <div className="bg-white flex  dark:bg-gray-800 pt-20 mt-32 rounded-xl shadow-lg shadow-indigo-500/50  ">
+        <div
+          className="
+            bg-gray-100 flex  dark:bg-gray-800 pt-10 mt-32 rounded-xl shadow-lg shadow-indigo-500/50  "
+        >
           <div className=" w-1/2 mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
             <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
               <span className="block text-6xl">I am Alexis</span>
@@ -36,26 +39,35 @@ const Home = () => {
           <img src={Bonome} alt="bonome" className="w-1/2 " />
         </div>
       </div>
-      <div className="pt-20 text-center">
-        <div>
-          <div className="title font-bold text-lg">
+      <div className="py-20 flex  justify-center ">
+        <div className="p-2  w-1/2 flex-col items-center justify-center pt-8 mt-8 border-t border-gray-200 ">
+          <div className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 text-center">
             <h2>What i do</h2>
           </div>
-          <div className="flex justify-center  gap-4 p-5">
-            <div className="w-1/3 p-5 text-black bg-white dark:bg-gray-800 rounded-xl shadow-lg shadow-indigo-500/50">
+          <div className="flex gap-4 ">
+            <div
+              className="w-1/3 p-5 text-black 
+            bg-gray-100 dark:bg-gray-800 rounded-xl shadow-lg shadow-indigo-500/50"
+            >
               <h2>Front end Developpement</h2>
               <p className="pt-4">
                 I make all the front end with React and Tailwind CSS.
               </p>
             </div>
-            <div className="w-1/3 p-5 text-black bg-white dark:bg-gray-800 rounded-xl shadow-lg shadow-indigo-500/50">
+            <div
+              className="w-1/3 p-5 text-black 
+            bg-gray-100 dark:bg-gray-800 rounded-xl shadow-lg shadow-indigo-500/50"
+            >
               <h2>Wed Design (A little)</h2>
               <p className="pt-4">
                 I make basic design for website inspired by existent design. I
-                am not a web designer but i try to make my best.
+                am not a web designer but i try my best.
               </p>
             </div>
-            <div className="w-1/3 p-5 text-black bg-white dark:bg-gray-800 rounded-xl shadow-lg shadow-indigo-500/50">
+            <div
+              className="w-1/3 p-5 text-black 
+            bg-gray-100 dark:bg-gray-800 rounded-xl shadow-lg shadow-indigo-500/50"
+            >
               <h2>Back end Developpement</h2>
               <p className="pt-4">
                 I make all the back end and create the api between the front and
