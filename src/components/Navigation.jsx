5484fc227d374dacc5a610d0ae79d0ebb2913655
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-15 18:10:48 +0100

    up

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,64 +3,50 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <div>
+    <header className="sticky top-0 z-50">
       <nav
         className="
             w-full
             py-4
             text-gray-500
-            dark:bg-gray-800
-            dark:decoration-sky-500
-            hover:dark:decoration-white hover:decoration-white
             "
       >
         <div className=" w-full flex items-center justify-between px-6">
-          <div
-            className=" flex items-center justify-between"
-            id="navbarSupportedContent"
-          >
+          <div className=" flex items-center justify-between">
             <div className="logo">
               <Link to="/" className="ttext-xl text-black font-semibold">
-                Alexis Noukan
+                <span className="text-2xl">AN</span>
               </Link>
             </div>
           </div>
-          <div>
-            <ul className="navbar-nav flex items-center justify-center  pl-0 list-style-none mr-auto">
-              <li className="nav-item px-2">
+          <div className="flex items-center gap-4">
+            <ul className="flex items-center justify-center gap-4 list-style-none ">
+              <li className="px-2">
                 <Link to="/" className="nav-link active" aria-current="page">
                   <span>Home</span>
                 </Link>
               </li>
-              <li className="nav-item px-2">
-                <Link
-                  to="/Project"
-                  className="nav-link active"
-                  aria-current="page"
-                >
+              <li className="px-2">
+                <Link to="/Project">
                   <span>Project</span>
                 </Link>
               </li>
-              <li className="nav-item px-2">
-                <Link
-                  to="/Knowledge"
-                  className="nav-link active"
-                  aria-current="page"
-                >
+              <li className="px-2">
+                <Link to="/Knowledge">
                   <span>Knowledge</span>
                 </Link>
               </li>
-            </ul>
-          </div>
-          <div className=" flex gap-5 items-center   ">
-            <Link to="/Contact">
-              <button className="btn bg-indigo-600">
-                <span>Contact now</span>
-              </button>
-            </Link>
+            </ul>{" "}
+            <div className=" flex gap-5 items-center   ">
+              <Link to="/Contact">
+                <button className="btn bg-indigo-600">
+                  <span>Contact now</span>
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </nav>
-    </div>
+    </header>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,33 +3,33 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <main className="dark:bg-slate-700">
+    <main>
       <section className="min-h-screen  ">
         <div className="  py-[10%] px-[10%]">
-          <article>
+          <article className=" flex flex-col gap-3">
             <p className="text-3xl">Hi! I am</p>
-            <h1 className="font-bold text-8xl sm:text-2xl"> Alexis NOUKAN</h1>
+            <h1 className="font-bold text-8xl "> Alexis NOUKAN</h1>
             <h2 className="text-5xl">A front end developper</h2>
             <h2 className="text-5xl">A backend developper</h2>
           </article>
         </div>
       </section>
-      <section className="flex gap-2 min-h-screen">
+      <section className="flex flex-wrap gap-2 min-h-screen">
         <div className="py-[10%] px-[10%]">
-          <article>
+          <article className="w-[20%]">
             <h2>Front end Developpement</h2>
             <p className="pt-4">
               I make all the front end with React and Tailwind CSS.
             </p>
           </article>
-          <article>
+          <article className="w-[20%]">
             <h2>Wed Design (A little)</h2>
             <p className="pt-4">
               I make basic design for website inspired by existent design. I am
               not a web designer but i try my best.
             </p>
           </article>
-          <article>
+          <article className="w-[20%]">
             <h2>Back end Developpement</h2>
             <p className="pt-4">
               I make all the back end and create the api between the front and
