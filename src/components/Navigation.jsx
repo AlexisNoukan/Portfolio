cd4738c3592c49f4d6a36d7ec6a6df477dc072f3
AlexisNoukan <alexisnoukan@gmail.com>
2023-02-25 23:19:28 +0100

    nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -65,7 +65,7 @@ export default function Navigation() {
   );
 
   return (
-    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+    <Navbar className="mx-auto max-w-screen mt-4 py-2 px-4 lg:px-8 lg:py-4">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
