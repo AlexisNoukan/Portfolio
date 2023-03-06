7acf6a79b7b69bced1ab627f30a6bbfff84e56c8
AlexisNoukan <alexisnoukan@gmail.com>
2023-03-06 16:40:38 +0100

    config tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
-  darkMode: "false",
-  content: ["./src/**/*.{js,jsx}"],
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        "regal-blue": "#243c5a",
-      },
-      fontFamily: {
-        sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
-});
+};
