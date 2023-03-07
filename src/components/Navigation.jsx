bf4448c4827ba83d9f3548c7ec72f068fa484f6f
AlexisNoukan <alexisnoukan@gmail.com>
2023-03-07 16:03:11 +0100

    up nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <header className="navbar text-primary-content">
+    <div className="navbar ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -23,10 +23,10 @@ const Navigation = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-500 rounded-box w-52"
           >
             <li>
-              <a>Project</a>
+              <a>Item 1</a>
             </li>
             <li tabIndex={0}>
               <a className="justify-between">
@@ -51,34 +51,48 @@ const Navigation = () => {
               </ul>
             </li>
             <li>
-              <a>Skills</a>
+              <a>Item 3</a>
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl rounded-full border-2 color-white">
-          AN
-        </a>
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Home</a>
+            <a>Item 1</a>
+          </li>
+          <li tabIndex={0}>
+            <a>
+              Parent
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
           </li>
           <li>
-            <a>Project</a>
-          </li>
-          <li>
-            <a>Skills</a>
-          </li>
-          <li>
-            <a>Knowledges</a>
+            <a>Item 3</a>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Contact Me</a>
+        <a className="btn">Get started</a>
       </div>
-    </header>
+    </div>
   );
 };
 
